ace19361198bb021a12537f9894b47a31317ce49
ken <k>
2019-09-05 20:10:26 +0200

    add cors -- unsafe but for demo purposes

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const employees = [{
     name: 'jeff'
 }];
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.get('/', (req, res) => {
     res.status(200).json(
