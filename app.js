704038fa0580399afdaf313ffdbf0be1763100d9
ken <k>
2019-09-05 20:45:33 +0200

    edit delete to make it work

--- app.js
+++ app.js
@@ -4,35 +4,46 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const employees = [];
+var employees = [];
+var idcounter = 1;
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
 
 
 app.get('/', (req, res) => {
+    console.log('GET: get all employees');
+
     res.status(200).json(
         employees
     )
 });
 
 app.get('/:id', (req, res) => {
+    console.log('GET: get employee with id ' + req.params.id);
+
     res.status(200).json(
-        employees.find(x => x.id === req.params.id)
+        employees.find(x => x.id + '' === req.params.id + '')
     )
 });
 
 app.post('/', (req, res) => {
-    employees.push(req.body);
+    console.log('POST: adding employee');
+
+    employees.push({ id: idcounter, ...req.body });
+    idcounter++;
     res.status(200).json({
         message: 'ok'
     })
 });
 
 app.put('/:id', (req, res) => {
+    console.log('PUT: editing employee');
+
     employees[employees.indexOf(employees.find(x => x.id === req.params.id))] = req.body
     res.status(200).json({
         message: 'ok'
@@ -40,9 +51,18 @@ app.put('/:id', (req, res) => {
 });
 
 app.delete('/:id', (req, res) => {
-    employees = employees.splice(employees.indexOf(employees.find(x => x.id === req.params.id)), 1);
+    console.log('DELETE: deleting employee with id: ' + req.params.id)
+    if (employees.length === 1) {
+        employees = [];
+    } else {
+        employees.splice(
+            employees.indexOf(employees.find(x => x.id + '' === req.params.id + '')),
+            1);
+    }
+
     res.status(200).json({
         message: 'ok'
     })
 });
+
 module.exports = app;
