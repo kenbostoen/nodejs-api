69264bff05ca0b0a76b553f053731b3e93332b05
ken <k>
2019-09-05 20:12:16 +0200

    remove testdata oops

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const employees = [{
-    id: "test",
-    name: 'jeff'
-}];
+const employees = [];
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
