6af62438db564ce31d716a677d54cf7a7f3daeeb
ken <k>
2019-09-05 20:02:43 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const employees = [{
+    id: "test",
+    name: 'jeff'
+}];
+
+
+app.get('/', (req, res) => {
+    res.status(200).json(
+        employees
+    )
+});
+
+app.get('/:id', (req, res) => {
+    res.status(200).json(
+        employees.find(x => x.id === req.params.id)
+    )
+});
+
+app.post('/', (req, res) => {
+    employees.push(req.body);
+    res.status(200).json({
+        message: 'ok'
+    })
+});
+
+app.put('/:id', (req, res) => {
+    employees[employees.indexOf(employees.find(x => x.id === req.params.id))] = req.body
+    res.status(200).json({
+        message: 'ok'
+    })
+});
+
+app.delete('/:id', (req, res) => {
+    employees = employees.splice(employees.indexOf(employees.find(x => x.id === req.params.id)), 1);
+    res.status(200).json({
+        message: 'ok'
+    })
+});
+module.exports = app;
