81f736944e50a6a5cf9b3b61005996de3957749f
ken <k>
2019-09-15 15:42:50 +0200

    fix accept all headers

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var idcounter = 1;
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     next();
 });
